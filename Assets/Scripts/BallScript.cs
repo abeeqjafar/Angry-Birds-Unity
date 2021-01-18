292481f9d8238697ba0fdd2f47cbf9e1459294a6
Abeeq <l164253@lhr.nu.edu.pk>
2021-01-18 16:23:33 +0500

    First commit

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,127 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BallScript : MonoBehaviour
+{
+    bool isPressed = false;
+    public Rigidbody2D rb;
+    public Rigidbody2D hookRb;
+    public float relaseTime = 0.15f;
+    public float maxDragDistance = 2f;
+    public GameObject Next;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isPressed)
+        {
+            Vector2 mpos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(Vector3.Distance(mpos, hookRb.position)<=maxDragDistance)
+            {
+                rb.position = mpos;
+            }
+            else
+            {
+                rb.position = (mpos - hookRb.position).normalized * maxDragDistance + hookRb.position;
+            }
+            if (EnemyScript.count == 0)
+            {
+                Debug.Log("You Won the Game!!!");
+                if (SceneManager.GetActiveScene().name == "Level1")
+                {
+                    SceneManager.LoadScene("Level2");
+
+                }
+                else if (SceneManager.GetActiveScene().name == "Level2")
+                {
+                    SceneManager.LoadScene("Level3");
+                }
+                else if (SceneManager.GetActiveScene().name == "Level3")
+                {
+                    SceneManager.LoadScene("Level1");
+                }
+            }
+        }
+        
+    }
+
+    private void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+        
+        StartCoroutine(release());
+    }
+
+    IEnumerator release()
+    {
+        yield return new WaitForSeconds(relaseTime);
+        GetComponent<SpringJoint2D>().enabled = false;
+        this.enabled = false;
+        yield return new WaitForSeconds(4f);
+        if(Next!=null)
+        {
+            Next.SetActive(true);
+            if (EnemyScript.count == 0)
+            {
+                Debug.Log("You Won the Game!!!");
+                if (SceneManager.GetActiveScene().name == "Level1")
+                {
+                    SceneManager.LoadScene("Level2");
+
+                }
+                else if (SceneManager.GetActiveScene().name == "Level2")
+                {
+                    SceneManager.LoadScene("Level3");
+                }
+                else if (SceneManager.GetActiveScene().name == "Level3")
+                {
+                    SceneManager.LoadScene("Level1");
+                }
+            }
+
+        }
+        else
+        {
+            if(EnemyScript.count>0)
+            {
+                Debug.Log("You Lost the Game!!!");
+                SceneManager.LoadScene("Level1");
+            }
+            else
+            {
+                Debug.Log("You Won the Game!!!");
+                if (SceneManager.GetActiveScene().name == "Level1")
+                {
+                    SceneManager.LoadScene("Level2");
+
+                }
+                else if(SceneManager.GetActiveScene().name == "Level2")
+                {
+                    SceneManager.LoadScene("Level3");
+                }
+                else if(SceneManager.GetActiveScene().name == "Level3")
+                {
+                    SceneManager.LoadScene("Level1");
+                }
+            }
+            
+            
+        }
+        
+    }
+}
---
+++ Assets/Scripts/EnemyScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour
+{
+    public float health = 4f;
+    public GameObject KillingEffect;
+    public static int count = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        count++;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.relativeVelocity.magnitude>=health)
+        {
+            die();
+        }
+    }
+
+    void die()
+    {
+        Instantiate(KillingEffect, transform.position, Quaternion.identity);
+        Destroy(this.gameObject);
+        count--;
+
+        //if(count==0)
+        //{
+        //    Debug.Log("Game Over...!!");
+        //}
+    }
+}
